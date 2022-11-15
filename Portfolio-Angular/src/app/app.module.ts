3ba9b10e109bffde3674b0a5c8d8b1a129b03c55
Adnan Sattikar <adnan.sattikar2001@gmail.com>
2022-11-15 11:25:40 +0530

    created components

--- Portfolio-Angular/src/app/app.module.ts
+++ Portfolio-Angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { PortfolioComponent } from './Components/portfolio/portfolio.component';
+import { BlogComponent } from './Components/blog/blog.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
